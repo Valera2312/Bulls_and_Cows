090e5a132693cc4d147e2cc536d0e7f159d2ddb1
Валерий Проскуряков <79242258075@yandex.ru>
2021-01-07 01:27:36 +1000

    Initial commit

---
+++ Problems/Initializing an array of characters/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char[] characters = {'a', 'z', 'e', 'd'};
+        //char[] array = new char[10000000000000];
+        char[] arrayE = new char[0];
+        char[] aSrray = new char[-1];
+        aSrray[0] = 'd';
+
+        System.out.println(Arrays.toString(aSrray));
+    }
+}
